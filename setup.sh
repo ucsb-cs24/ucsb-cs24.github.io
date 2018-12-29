42339627116e146df0d9c1416b696762d5d9f094
Phill Conrad <pconrad@cs.ucsb.edu>
2018-12-29 09:08:12 -0800

    update to Ruby 2.5.1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing software needed to run Jekyll locally... "
-rvm install ruby-2.1.7
+rvm install ruby-2.5.1
 gem install bundler
 bundle install --path vendor/bundle
 echo "Done."
